ee2b1aba3815b8cf814a366b9d48df18791eb25c
PanjiDenisGiantoro <panjidenisgiantoroo@gmail.com>
2023-12-27 18:08:30 +0700

    include photo in section all

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8" />
-    <title>Amalia - Responsive Bootstrap 4 Personal Website Template</title>
+    <title>Panji Denis Giantoro</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport" />
     <meta content="" name="description" />
     <meta content="" name="keywords" />
